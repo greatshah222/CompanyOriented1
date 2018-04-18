6bf95f0a05ef64a6f3554e8af7cf1ab9520e3c81
awaleRichard <t3awri00@students.oamk.fi>
2018-04-18 20:07:55 +0300

    menu file css added TBD (extra css)

--- resources/views/front/menu.blade.php
+++ resources/views/front/menu.blade.php
@@ -3,35 +3,63 @@
 @section('content')'
 <br>
 <br>
-<br>
-<br><br>
-<br>
-
-
-    <div class="row">
-        <div class="small-5 small-offset-1 columns">
-            <div class="item-wrapper">
-                <div class="img-wrapper">
-                    <a href="#">
-                        <img src="https://github.com/greatshah222/Software_development/blob/firstPhase/2.jpg?raw=true"/>
-                    </a>
+<section class="most_popular_item_area menu_list_page">
+            <div class="container">
+                <div class="popular_filter">
+                    <ul>
+                        <li class="active" data-filter="*"><a href="">All</a></li>
+                        <li data-filter=".break"><a href="">Breakfast</a></li>
+                        <li data-filter=".lunch"><a href="">Lunch</a></li>
+                        <li data-filter=".dinner"><a href="">Dinner</a></li>
+                        <li data-filter=".snacks"><a href="">Snacks</a></li>
+                        <li data-filter=".coffee"><a href="">Coffee</a></li>
+                    </ul>
+                </div>
+                <div class="p_recype_item_main">
+                    <div class="row p_recype_item_active">
+                        <div class="col-md-6 break snacks">
+                            <div class="media">
+                                <div class="media-left">
+                                <img src="{{asset('dist/images/recype/recype-1.jpg')}}" alt='what'>
+                                </div>
+                                <div class="media-body">
+                                    <a href="#"><h3>grass Flavour</h3></a>
+                                    <h4>$32</h4>
+                                    <p>Lorem ipsum dolor sit amets, consectetur adipiscing </p>
+                                    <a class="read_mor_btn" href="#">Add To Cart</a>
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star-half-o"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 break coffee">
+                            <div class="media">
+                                <div class="media-left">
+                                    <img src="{{asset('dist/images/recype/recype-2.jpg')}}" alt="">
+                                </div>
+                                <div class="media-body">
+                                    <a href="#"><h3>Vegetable Flavour</h3></a>
+                                    <h4>$32</h4>
+                                    <p>Lorem ipsum dolor sit amets, consectetur adipiscing </p>
+                                    <a class="read_mor_btn" href="#">Add To Cart</a>
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-star-half-o"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="small-6 columns">
-            <div class="item-wrapper">
-                <h3 class="subheader">
-                    <span class="price-tag"></span> Laravel PhP Framework
-                </h3>
-                <div class="row">
-
-                    <a href="#" class="button  expanded">View Material</a>
-                </div>
-
-
-
-            </div>
-        </div>
-    </div>
-    </div>
+        </section>
+                        
 @endsection
